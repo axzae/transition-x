36e60bd976b47e673b29b647cbd1353b45ff9f77
You Qi <youqi@axzae.com>
2023-07-04 21:35:48 +0800

    Setup github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,7 @@ buildscript {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.6.0")
     }
 }
+
+tasks.register("preMerge") {
+    description = "Runs all the tests/verification tasks on both top level and included build."
+}
